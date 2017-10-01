afbce61db2545a2e6c5510ab7a303aa919e6ad62
Bernard Strong <bstrong71@gmail.com>
2017-09-30 23:33:05 -0400

    added all pages; still working on dynamic id for view page and adding new customer

--- src/main/java/com/example/customer/service/CustomerController.java
+++ src/main/java/com/example/customer/service/CustomerController.java
@@ -4,8 +4,9 @@ import com.example.customer.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CustomerController {
@@ -13,11 +14,29 @@ public class CustomerController {
     @Autowired
     CustomerService customerService;
 
-    @RequestMapping(path="/all_customers", method = RequestMethod.GET)
+    @RequestMapping("/all_customers")
     public String allCustomers(Model model) {
         model.addAttribute("customers", customerService.get());
         return "all_customers";
     }
 
+    @RequestMapping("/add_customer")
+    public String addCustomer(@RequestParam(value="firstName") String firstName, @RequestParam(value="lastName") String lastName, @RequestParam(value="phone") String phone, @RequestParam(value="email") String email, Model model) {
+        Customer customer = new Customer();
 
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        customer.setPhone(phone);
+        customer.setEmail(email);
+        model.addAttribute(customer);
+
+        return "all_customers";
+    }
+
+    @RequestMapping("/view_customer/{id}")
+    public String viewCustomer(@PathVariable(value = "id") int customerId, Model model ) {
+
+        model.addAttribute("customers", customerService.getById(customerId));
+        return "view_customer";
+    }
 }
